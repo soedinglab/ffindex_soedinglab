e6eb79dd9a925610ae8a135ab6bc828959ef6119
Milot Mirdita <milot@mirdita.de>
2017-11-22 13:03:54 +0100

    poll() based applt_mpi, that should not be able to deadlock on large entries anymore

--- src/ffindex_apply_mpi.c
+++ src/ffindex_apply_mpi.c
@@ -1,17 +1,19 @@
 /* 
- * FFindex
- * written by Andy Hauser <hauser@genzentrum.lmu.de>,
- * and Milot Mirdita <milot@mirdita.de>
- * Please add your name here if you distribute modified versions.
+ * ffindex_apply_mpi
+ * Apply a program to each FFindex entry
  *
+ * Rewritten by Milot Mirdita <milot@mirdita.de>
+ * New implementation licensed under GPLv3
+ * See https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * Uses parts of gnulib
+ * https://www.gnu.org/software/gnulib/
+ * Licensed under GPLv3
+ *
+ * Original implementation written by Andy Hauser <hauser@genzentrum.lmu.de>,
  * FFindex is provided under the Create Commons license "Attribution-ShareAlike
  * 3.0", which basically captures the spirit of the Gnu Public License (GPL).
- *
- * See:
- * http://creativecommons.org/licenses/by-sa/3.0/
- *
- * ffindex_apply_mpi
- * apply a program to each FFindex entry
+ * See http://creativecommons.org/licenses/by-sa/3.0/
  */
 
 #define _GNU_SOURCE 1
@@ -40,181 +42,228 @@
 #include "mpq/mpq.h"
 #endif
 
-extern char **environ;
+// Analogous to gnulib implementation
+pid_t create_pipe(const char *prog_path, char **prog_argv, char **environ, int fd[2]) {
+    int ifd[2];
+    int ofd[2];
+
+    int err;
+    if ((err = pipe(ifd)) < 0) {
+        perror("pipe ifd");
+        errno = err;
+        return -1;
+    }
+    if ((err = pipe(ofd)) < 0) {
+        perror("pipe ofd");
+        errno = err;
+        return -1;
+    }
+    
+    int actions_allocated = 0;
+    int attrs_allocated = 0;
+    
+    posix_spawn_file_actions_t actions;
+    posix_spawnattr_t attrs;
+    pid_t child;
+    
+    if ((err = posix_spawn_file_actions_init(&actions)) != 0
+          || (actions_allocated = 1,
+             (err = posix_spawn_file_actions_adddup2 (&actions, ofd[0], STDIN_FILENO)) != 0
+          || (err = posix_spawn_file_actions_adddup2 (&actions, ifd[1], STDOUT_FILENO)) != 0
+          || (err = posix_spawn_file_actions_addclose(&actions, ofd[0])) != 0
+          || (err = posix_spawn_file_actions_addclose(&actions, ifd[1])) != 0
+          || (err = posix_spawn_file_actions_addclose(&actions, ofd[1])) != 0
+          || (err = posix_spawn_file_actions_addclose(&actions, ifd[0])) != 0
+#ifdef POSIX_SPAWN_USEVFORK
+          || ((err = posix_spawnattr_init(&attrs)) != 0
+                || (attrs_allocated = 1,
+                   (err = posix_spawnattr_setflags(&attrs, POSIX_SPAWN_USEVFORK)) != 0))
+#endif
+          || (err = posix_spawnp(&child, prog_path, &actions, attrs_allocated ? &attrs : NULL, prog_argv, environ)) != 0))
+    {
+        if (actions_allocated) {
+            posix_spawn_file_actions_destroy(&actions);
+        }
+        if (attrs_allocated) {
+            posix_spawnattr_destroy(&attrs);
+        }
+        
+        close(ifd[0]);
+        close(ifd[1]);
+        close(ofd[0]);
+        close(ofd[1]);
+        errno = err;
+        return -1;
+    }
+
+    posix_spawn_file_actions_destroy(&actions);
+    if (attrs_allocated) {
+        posix_spawnattr_destroy(&attrs);
+    }
+
+    if ((err = close(ofd[0])) == -1 || (err = close(ifd[1])) == -1) {
+        errno = err;
+        return -1;
+    }
+
+    fd[0] = ifd[0];
+    fd[1] = ofd[1];
+    return child;
+}
 
 int
-ffindex_apply_by_entry(char *data, ffindex_entry_t *entry, char *program_name, char **program_argv,
+ffindex_apply_by_entry(char *data, ffindex_entry_t *entry, char *program_name, char **program_argv, char **environ,
                        FILE *data_file_out, FILE *index_file_out, FILE *log_file_out, size_t *offset, int quiet) {
-    const int capture_stdout = (data_file_out != NULL && index_file_out != NULL);
-
-    int pipefd_stdin[2];
-    if (pipe(pipefd_stdin) != 0) {
-        perror(entry->name);
-        return errno;
-    }
-
-    int pipefd_stdout[2];
-    if (capture_stdout && pipe(pipefd_stdout) != 0) {
-        perror(entry->name);
-        return errno;
-    }
-
-    posix_spawn_file_actions_t factions;
-    if (posix_spawn_file_actions_init(&factions) != 0) {
-        perror("posix_spawn_file_actions_init");
-        return errno;
-    }
-    if (posix_spawn_file_actions_addclose(&factions, pipefd_stdin[1]) != 0) {
-        perror("posix_spawn_file_actions_addclose");
-        return errno;
-    }
-    if (posix_spawn_file_actions_adddup2(&factions, pipefd_stdin[0], fileno(stdin)) != 0) {
-        perror("posix_spawn_file_actions_adddup2");
-        return errno;
-    }
-
-    if (capture_stdout && posix_spawn_file_actions_addclose(&factions, pipefd_stdout[0]) != 0) {
-        perror("posix_spawn_file_actions_addclose");
-        return errno;
-    }
-    if (capture_stdout && posix_spawn_file_actions_adddup2(&factions, pipefd_stdout[1], fileno(stdout)) != 0) {
-        perror("posix_spawn_file_actions_adddup2");
-        return errno;
-    }
-
-    posix_spawnattr_t attr;
-    if (posix_spawnattr_init(&attr) != 0) {
-        perror("posix_spawnattr_init");
-        return errno;
-    }
-
-#ifdef POSIX_SPAWN_USEVFORK
-    short spawnFlags = 0;
-    spawnFlags |= POSIX_SPAWN_USEVFORK;
-    if (posix_spawnattr_setflags(&attr, spawnFlags) != 0) {
-        perror("posix_spawnattr_setflags");
-        return errno;
-    }
-#endif
+    const int ignore_stdout = (data_file_out == NULL) || (index_file_out == NULL);
 
     struct timeval tv;
     time_t start = 0, end = 0;
-    if (!quiet) {
+    if (!quiet && log_file_out != NULL) {
         gettimeofday(&tv, NULL);
         start = (tv.tv_sec) * 1000LL + (tv.tv_usec) / 1000;
     }
 
-    pid_t child_pid;
-    setenv("FFINDEX_ENTRY_NAME", entry->name, 1);
-    if (posix_spawnp(&child_pid, program_name, &factions, &attr, program_argv, environ)) {
-        perror("posix_spawnp");
-        return errno;
-    }
-
-    // Read end is for child only
-    if (close(pipefd_stdin[0]) == -1) {
-        perror("close stdin0");
-        return errno;
-   }
-
-    if (capture_stdout && close(pipefd_stdout[1]) == -1) {
-        perror("close stdout1");
-        return errno;
-    }
-
     size_t start_offset = *offset;
     char *file_data = ffindex_get_data_by_entry(data, entry);
+	if (file_data == NULL) {
+		return -1;
+	}
+
+	// only works with the environ we construct ourselves
+    // local_environment() leaves the first element free to use for ourselves
+	snprintf(environ[0], 64, "FFINDEX_ENTRY_NAME=%s", entry->name);
+
+    int write_closed = 0;
+	int fd[2];
+    pid_t child_pid;
+    if ((child_pid = create_pipe(program_name, program_argv, environ, fd)) == -1) {
+		perror("create_pipe");
+		return errno;     
+	}
+
+    // Analogous to gnulib implementation
+	int status = 0;
+	int fcntl_flags;
+	if ( (fcntl_flags = fcntl(fd[1], F_GETFL, 0)) < 0
+	  || fcntl(fd[1], F_SETFL, fcntl_flags | O_NONBLOCK) == -1
+	  || (fcntl_flags = fcntl(fd[0], F_GETFL, 0)) < 0
+	  || fcntl(fd[0], F_SETFL, fcntl_flags | O_NONBLOCK) == -1)
+	{
+		goto fail;
+	}
 
     char buffer[PIPE_BUF];
     size_t to_write = entry->length - 1;
     size_t written = 0;
 
     struct pollfd plist[2];
-    plist[0].fd = capture_stdout ? pipefd_stdout[0] : -1;
-    plist[1].fd = pipefd_stdin[1];
     for (;;) {
-        plist[0].events = POLLIN;
-        plist[0].revents = 0;
-
         size_t rest = to_write - written;
         size_t batch_size = PIPE_BUF;
         if (rest < PIPE_BUF) {
             batch_size = rest;
         }
- 
-        plist[1].events = rest > 0 ? POLLOUT : 0;
+
+        plist[0].fd = write_closed == 0 ? fd[1] : fd[1] * -1;
+        plist[0].events = POLLOUT;
+        plist[0].revents = 0;
+
+        plist[1].fd = fd[0];
+        plist[1].events = POLLIN;
         plist[1].revents = 0;
 
         if (poll(plist, 2, -1) == -1) {
             if (errno == EAGAIN) {
-                usleep(1);
+                perror("again");
                 continue;
             }
             perror("poll");
-            return errno;
+            status = errno;
+            break;
         }
 
-        if (plist[0].revents & POLLIN) {
-            //fprintf(stderr, "read  %d %d\n", plist[0].events, plist[1].events);
-            //fflush(stderr);
-            ssize_t bytes_read = read(pipefd_stdout[0], &buffer, sizeof(buffer));
-            if (bytes_read == -1) {
-                perror("read stdout0");
-                return errno;
+        if (plist[0].revents & POLLOUT) {
+			ssize_t write_size = batch_size;
+            if (to_write - written > 0) {
+				for(;;) {
+					ssize_t w = write(fd[1], file_data + written, write_size);
+					if (w < 0) {
+						if (errno != EAGAIN) {
+							perror("write stdin1");
+                            status = errno;
+                            goto fail;
+						} else {
+							write_size = write_size / 2;
+							if (write_size == 0) {
+								break;
+							}
+						}
+					} else {
+						written += w;
+						break;
+					}
+				}
+			} else {
+                if (close(fd[1]) == -1) {
+                    perror("close error");
+                    status = errno;
+                    break;
+                }
+                write_closed = 1;
             }
-            ffindex_insert_memory_add(data_file_out, offset, buffer, bytes_read);
-        } else if (plist[1].revents & POLLOUT) {
-            //fprintf(stderr, "write %d %d\n", plist[0].events, plist[1].events);
-            //fflush(stderr);
-            ssize_t w = write(pipefd_stdin[1], file_data + written, batch_size);
-            if (w == -1) {
-                perror("write stdin1");
-                return errno;
+        } else if (plist[1].revents & POLLIN) {
+            ssize_t bytes_read = read(plist[1].fd, &buffer, sizeof(buffer));
+			if (bytes_read > 0) {
+                if (ignore_stdout != 0) {
+                    ffindex_insert_memory_add(data_file_out, offset, buffer, bytes_read);
+                }
+			} else if (bytes_read < 0) {
+				if (errno != EAGAIN) {
+					perror("read stdout0");
+                    status = errno;
+                    break;
+				}
+            } else if (bytes_read == 0 && write_closed == 1) {
+				break;
             }
-            written += w;
         } else {
             // nothing left to read or write
-            fprintf(stderr, "exit %d %d\n", plist[0].revents, plist[1].revents);
-            fflush(stderr);
             break;
         }
     }
-    ffindex_insert_memory_end(data_file_out, index_file_out, start_offset, offset, entry->name);
-    fprintf(stderr, "out");
-    fflush(stderr);
 
-    if (close(pipefd_stdin[1]) == -1) {
-        perror("close stdin1");
-        return errno;
+fail:
+    if (ignore_stdout != 0) {
+        ffindex_insert_memory_end(data_file_out, index_file_out, start_offset, offset, entry->name);
     }
 
-    if (capture_stdout && close(pipefd_stdout[0]) == -1) {
-        perror("close stdout0");
-        return errno;
+    if (write_closed == 0) {
+        close(fd[1]);
     }
 
-    int status;
+    if (close(fd[0]) == -1) {
+        perror("close stdout");
+        status = errno;
+    }
+
     if (waitpid(child_pid, &status, 0) == -1) {
         perror("waitpid");
-        return errno;
+        status = errno;
     }
 
-    if (capture_stdout) {
+    if (ignore_stdout != 0) {
         fflush(data_file_out);
         fflush(index_file_out);
     }
 
-    if (!quiet) {
+    if (!quiet && log_file_out != NULL) {
         gettimeofday(&tv, NULL);
         end = (tv.tv_sec) * 1000LL + (tv.tv_usec) / 1000;
-        fprintf(log_file_out, "%s\t%ld\t%ld\t%ld\t%d\n", entry->name, entry->offset, entry->length, end - start,
-                WEXITSTATUS(status));
+        fprintf(log_file_out, "%s\t%ld\t%ld\t%ld\t%d\n", 
+                entry->name, entry->offset, entry->length, end - start, WEXITSTATUS(status));
     }
 
-    posix_spawnattr_destroy(&attr);
-    posix_spawn_file_actions_destroy(&factions);
-
-    return EXIT_SUCCESS;
+    return status;
 }
 
 #ifdef HAVE_MPI
@@ -227,6 +276,7 @@ struct ffindex_apply_mpi_data_s {
     FILE *log_file_out;
     char *program_name;
     char **program_argv;
+	char **environ;
     int quiet;
     size_t offset;
 };
@@ -242,6 +292,7 @@ void ffindex_apply_worker_payload(void *pEnv, const size_t start, const size_t e
         int error = ffindex_apply_by_entry(env->data, entry,
                                            env->program_name,
                                            env->program_argv,
+                                           env->environ,
                                            env->data_file_out,
                                            env->index_file_out,
                                            env->log_file_out,
@@ -255,7 +306,6 @@ void ffindex_apply_worker_payload(void *pEnv, const size_t start, const size_t e
 
     return;
 }
-
 #endif
 
 void ignore_signal(int signal) {
@@ -286,6 +336,37 @@ void usage() {
                     "\tINDEX_FILENAME\t\tInput ffindex index file.\n"
                     "\tPROGRAM [PROGRAM_ARGS]\tProgram to be executed for every ffindex entry.\n"
     );
+}
+
+// Duplicate the process environment with one additional element at first position
+// Multiple threads can write safely into their own environ
+// There they can set the current FFINDEX_ENTRY_NAME and pass that to the child process
+extern char **environ;
+
+int prefix(const char *pre, const char *str) {
+        return strncmp(pre, str, strlen(pre)) == 0;
+}
+
+char** local_environment() {
+    size_t env_size = 0;
+    for (size_t i = 0; environ[i] != NULL; ++i) {
+        env_size++;
+    }
+
+    char** local_environ = (char**)malloc(sizeof(char*) * env_size + 2);
+    size_t j = 0;
+    local_environ[j++] = (char*)malloc(sizeof(char) * 64);
+    for (size_t i = 0; environ[i] != NULL; ++i) {
+        if (prefix("FFINDEX_ENTRY_NAME=", environ[i]) == 0) {
+            local_environ[j++] = environ[i];
+        }
+    }
+    local_environ[j] = NULL;
+}
+
+void free_local_environment(char** local_environ) {
+    free(local_environ[0]);
+    free(local_environ);
 }
 
 int main(int argn, char **argv) {
@@ -421,11 +502,13 @@ int main(int argn, char **argv) {
     int mpq_status = MPQ_Init(argn, argv, index->n_entries);
     if (mpq_status == MPQ_SUCCESS) {
         if (MPQ_rank != MPQ_MASTER) {
+			char **local_environ = local_environment();
             ffindex_apply_mpi_data_t *env = malloc(sizeof(ffindex_apply_mpi_data_t));
             env->data = data;
             env->index = index;
             env->program_name = program_name;
             env->program_argv = program_argv;
+			env->environ = local_environ;
             env->quiet = quiet;
             env->offset = 0;
 
@@ -497,6 +580,7 @@ int main(int argn, char **argv) {
             }
             cleanup_4:
             free(env);
+			free_local_environment(local_environ);
 
             MPI_Barrier(MPI_COMM_WORLD);
         } else {
@@ -514,54 +598,50 @@ int main(int argn, char **argv) {
     }
 #else
     FILE* data_file_out = NULL;
-    if (data_filename_out != NULL)
-    {
+    if (data_filename_out != NULL) {
         char data_filename_out_rank[FILENAME_MAX];
         snprintf(data_filename_out_rank, FILENAME_MAX, "%s", data_filename_out);
 
         data_file_out = fopen(data_filename_out_rank, "w+");
-        if (data_file_out == NULL)
-        {
+        if (data_file_out == NULL) {
             fferror_print(__FILE__, __LINE__, "ffindex_apply_worker_payload", data_filename_out);
             return EXIT_FAILURE;
         }
     }
 
     FILE* index_file_out = NULL;
-    if (index_filename_out != NULL)
-    {
+    if (index_filename_out != NULL) {
         char index_filename_out_rank[FILENAME_MAX];
         snprintf(index_filename_out_rank, FILENAME_MAX, "%s", index_filename_out);
 
         index_file_out = fopen(index_filename_out_rank, "w+");
-        if (index_file_out == NULL)
-        {
+        if (index_file_out == NULL) {
             fferror_print(__FILE__, __LINE__, "ffindex_apply_worker_payload", index_filename_out);
             return EXIT_FAILURE;
         }
     }
 
-    size_t offset = 0;
-    for (size_t i = 0; i < index->n_entries; i++)
-    {
-        ffindex_entry_t *entry = ffindex_get_entry_by_index(index, i);
-        if (entry == NULL)
-        {
-            exit_status = errno;
-            break;
-        }
+	char **local_environ = local_environment();
+	size_t offset = 0;
 
-        int error = ffindex_apply_by_entry(data, entry,
-                                           program_name, program_argv,
-                                           data_file_out, index_file_out, stdout,
-                                           &offset, quiet);
-        if (error != 0)
-        {
-            perror(entry->name);
-            exit_status = errno;
-            break;
-        }
-    }
+	for (size_t i = 0; i < index->n_entries; i++) {
+		ffindex_entry_t *entry = ffindex_get_entry_by_index(index, i);
+		if (entry == NULL) {
+			exit_status = errno;
+			break;
+		}
+
+		int error = ffindex_apply_by_entry(data, entry,
+										   program_name, program_argv, local_environ,
+										   data_file_out, index_file_out, stdout,
+										   &offset, quiet);
+		if (error != 0) {
+			perror(entry->name);
+			exit_status = errno;
+			break;
+		}
+	}
+	free_local_environment(local_environ);
 
     if (index_file_out) {
         fclose(index_file_out);
